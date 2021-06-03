38a51d7be65983e3133de80fe51db7d790407fe4
MaKyuSeok <mks502@naver.com>
2021-06-03 18:04:19 +0900

    docs : Swaager Update

--- src/main/java/com/mks/membershipservice/config/SwaggerConfig.java
+++ src/main/java/com/mks/membershipservice/config/SwaggerConfig.java
@@ -2,18 +2,39 @@ package com.mks.membershipservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any()).build();
+		ParameterBuilder aParameterBuilder = new ParameterBuilder();
+		aParameterBuilder.name("X-AUTH-TOKEN") //헤더 이름
+				.description("Access Tocken") //설명
+				.modelRef(new ModelRef("string"))
+				.parameterType("header")
+				.required(false)
+				.build();
+
+		List<Parameter> aParameters = new ArrayList<>();
+		aParameters.add(aParameterBuilder.build());
+		return new Docket(DocumentationType.SWAGGER_2)
+				.globalOperationParameters(aParameters)
+				.select()
+				.apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any())
+				.build();
+
 	}
 }
